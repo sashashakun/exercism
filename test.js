8326f62316d14da87b65edc7de9325ac53d498b3
sashashakun <alx.safari@gmail.com>
2016-03-13 23:51:54 +0300

    add testing environment

---
+++ test.js
@@ -0,0 +1,57 @@
+const exec = require('child_process').exec;
+
+exec('jasmine-node ./javascript/bob/bob.spec.js', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+})
+
+exec('jasmine-node ./javascript/bob/bob.spec.js', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+})
+
+exec('jasmine-node ./javascript/hello-world/hello-world.spec.js', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+})
+
+exec('jasmine-node ./javascript/hamming/hamming.spec.js', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+})
+
+exec('jasmine-node ./javascript/gigasecond/gigasecond.spec.js', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+})
+
+exec('jasmine-node ./javascript/leap/leap.spec.js', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+})
+
+exec('jasmine-node ./javascript/rna-transcrption/rna-transcrption.spec.js', (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+})
